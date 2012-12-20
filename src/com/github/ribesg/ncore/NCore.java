3c46e6d1048a6c38180a28f7512d951805b5ebc0
Ribesg <Ribesg@Dragon>
2012-12-20 22:45:19 +0100

    -

--- src/com/github/ribesg/ncore/NCore.java
+++ src/com/github/ribesg/ncore/NCore.java
@@ -6,25 +6,24 @@ import lombok.Setter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import com.github.ribesg.ncore.nodes.cuboid.CuboidNode;
+import com.github.ribesg.ncore.nodes.dodgeball.DodgeBallNode;
+import com.github.ribesg.ncore.nodes.general.GeneralNode;
 import com.github.ribesg.ncore.nodes.punisher.PunisherNode;
 
 public class NCore extends JavaPlugin {
 
-	@Getter
-	@Setter
-	private CuboidNode cuboidNode;
+    @Getter @Setter private CuboidNode    cuboidNode;
+    @Getter @Setter private DodgeBallNode dodgeBallNode;
+    @Getter @Setter private GeneralNode   generalNode;
+    @Getter @Setter private PunisherNode  punisherNode;
 
-	@Getter
-	@Setter
-	private PunisherNode punisherNode;
+    @Override
+    public void onEnable() {
+        // Nothing yet
+    }
 
-	@Override
-	public void onEnable() {
-		// Nothing yet
-	}
-
-	@Override
-	public void onDisable() {
-		// Nothing yet
-	}
+    @Override
+    public void onDisable() {
+        // Nothing yet
+    }
 }
