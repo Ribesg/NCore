96b552b017c491625a0b957b61cc6dc0605387ac
Ribesg <Ribesg@Dragon>
2012-12-20 20:09:43 +0100

    Added some nodes

---
+++ src/com/github/ribesg/ncore/nodes/dodgeball/DodgeBallNode.java
@@ -0,0 +1,7 @@
+package com.github.ribesg.ncore.nodes.dodgeball;
+
+import com.github.ribesg.ncore.nodes.Node;
+
+public interface DodgeBallNode extends Node {
+
+}
---
+++ src/com/github/ribesg/ncore/nodes/general/GeneralNode.java
@@ -0,0 +1,7 @@
+package com.github.ribesg.ncore.nodes.general;
+
+import com.github.ribesg.ncore.nodes.Node;
+
+public interface GeneralNode extends Node {
+
+}
